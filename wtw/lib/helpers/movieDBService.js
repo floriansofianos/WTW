f91de267be19680eb2cdbca55434f1feb449d677
Florian <florian.sofianos@fonebox.com.au>
2017-08-08 16:06:56 +1000

    Create a service for MovieDB

---
+++ wtw/lib/helpers/movieDBService.js
@@ -0,0 +1,13 @@
+﻿var mdb = require('moviedb')('d03322a5a892ce280f22234584618e9e');
+
+module.exports = function () {
+    var getRandomTenMovies = function () {
+        mdb.searchMovie({ query: 'Alien' }, (err, data) => {
+            var test = data;
+        });
+    };
+
+    return {
+        getRandomTenMovies: getRandomTenMovies
+    }
+}
--- wtw/lib/routes/testRoutes.js
+++ wtw/lib/routes/testRoutes.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mdb = require('moviedb')('d03322a5a892ce280f22234584618e9e');
 
 var testRoutes = function () {
     var testRouter = express.Router();
@@ -7,13 +6,6 @@ var testRoutes = function () {
     testRouter.route('/')
         .get(function (req, res) {
             res.json({ test: 'Hello World From API' });
-        });
-
-    testRouter.route('/movieDB')
-        .get(function (req, res) {
-            mdb.searchMovie({ query: 'Alien' }, (err, data) => {
-                res.json(data);
-            });
         });
 
     return testRouter;
