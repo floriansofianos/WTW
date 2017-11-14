8fcff14e00bc9c5b04a46050e3b7b4e9f3a9e5b7
Florian <florian.sofianos@fonebox.com.au>
2017-11-14 16:15:25 +1000

    First Commit on the help with the paging on movieDB

---
+++ wtw/lib/helpers/movieDBRequestHelper.js
@@ -0,0 +1,11 @@
+﻿var mdb = require('moviedb')('d03322a5a892ce280f22234584618e9e');
+
+module.exports = function () {
+    var makeRequest = function (method, page, query) {
+
+    }
+
+    return {
+        makeRequest: makeRequest
+    }
+}
