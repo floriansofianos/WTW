95e805649bf0857c87f3b98b972ea8721248cf78
Florian Sofianos <florian.sofianos@gmail.com>
2018-02-02 22:48:55 +1000

    Finish calculating score

--- wtw/lib/helpers/tvRecommandationService.js
+++ wtw/lib/helpers/tvRecommandationService.js
@@ -55,20 +55,20 @@ var tvRecommandationService = function() {
         }
     };
 
-    /*var getScore = function(userId, movieDBId, userProfileService, movieDBService, done) {
+    var getScore = function(userId, movieDBId, userProfileService, tvCacheService, done) {
         userProfileService.getAll(userId, function(err, profiles) {
             // filter out irrelevant profiles
             profiles = _.filter(profiles, function(p) { return p.scoreRelevance > 75 });
             // Get the selected movie
-            movieDBService.getMovieWithAdditionalInfo(movieDBId, 'en', function(err, selectedMovie) {
+            tvCacheService.get(movieDBId, function(err, selectedTVShow) {
                 // filter the relevant profiles
-                var directorIds = _.map(selectedMovie.directors, 'id');
-                var genreIds = _.map(selectedMovie.genres, 'id');
-                var writerIds = _.map(selectedMovie.writers, 'id');
-                var actorIds = _.map(selectedMovie.actors, 'id');
+                var creatorIds = _.map(selectedTVShow.tvShowInfo.created_by, 'id');
+                var genreIds = _.map(selectedTVShow.tvShowInfo.genres, 'id');
+                var writerIds = _.map(_.filter(selectedTVShow.tvShowCredits.crew, function (c) { return (c.job == 'Writer' || c.job == 'Screenplay'); }), 'id');
+                var actorIds = _.map(selectedTVShow.tvShowCredits.cast, 'id');
                 var country = selectedMovie.original_language;
 
-                var directorProfiles = _.filter(profiles, function(p) { return p.directorId && _.contains(directorIds, p.directorId); });
+                var creatorProfiles = _.filter(profiles, function (p) { return p.creatorId && _.contains(creatorIds, p.creatorId); });
                 var genreProfiles = _.filter(profiles, function (p) { return p.genreId && _.contains(genreIds, p.genreId); });
                 // Modify the genre profiles a bit: if we watch a lot of some genres, the score needs to be higher than it is
                 var seenCountForGenres = _.reduce(_.filter(profiles, function (p) { return p.genreId; }), function (memo, p) { return memo + p.seenCount; }, 0);
@@ -84,30 +84,30 @@ var tvRecommandationService = function() {
                 var actorProfiles = _.filter(profiles, function (p) { return p.castId && _.contains(actorIds, p.castId); });
                 var countryProfile = _.filter(profiles, function (p) { return p.country && p.country == country; });
 
-                var allComments = _.map(directorProfiles.concat(genreProfiles.concat(writerProfiles.concat(actorProfiles.concat(countryProfile)))), function(p) {
-                    var type = p.directorId ? 'DIRECTOR' : (p.writerId ? 'WRITER' : (p.genreId ? 'GENRE' : (p.country ? 'COUNTRY' : 'ACTOR')));
+                var allComments = _.map(creatorProfiles.concat(genreProfiles.concat(writerProfiles.concat(actorProfiles.concat(countryProfile)))), function(p) {
+                    var type = p.creatorId ? 'CREATOR' : (p.writerId ? 'WRITER' : (p.genreId ? 'GENRE' : (p.country ? 'COUNTRY' : 'ACTOR')));
                     if (p.score > 75) return { level: 2, name: p.name, type: type };
                     else if (p.score > 60) return { level: 1, name: p.name, type: type };
                     else if (p.score < 25 && type != 'GENRE' && type != 'ACTOR' && type != 'COUNTRY') return { level: -2, name: p.name, type: type };
                     else if (p.score < 40 && type != 'GENRE' && type != 'ACTOR' && type != 'COUNTRY') return { level: -1, name: p.name, type: type };
                 });
                 allComments = _.filter(allComments, function(c) { return c; });
-                var averageDirectors = directorProfiles.length > 0 ? (_.reduce(_.map(directorProfiles, 'score'), function(memo, p) { return memo + p }, 0)) / (_.size(directorProfiles)) : 0;
+                var averageCreators = creatorProfiles.length > 0 ? (_.reduce(_.map(creatorProfiles, 'score'), function (memo, p) { return memo + p }, 0)) / (_.size(creatorProfiles)) : 0;
                 var averageGenres = genreProfiles.length > 0 ? (_.reduce(_.map(genreProfiles, 'score'), function(memo, p) { return memo + p }, 0)) / (_.size(genreProfiles)) : 0;
                 var averageWriters = writerProfiles.length > 0 ? (_.reduce(_.map(writerProfiles, 'score'), function(memo, p) { return memo + p }, 0)) / (_.size(writerProfiles)) : 0;
                 var averageActors = actorProfiles.length > 0 ? (_.reduce(_.map(actorProfiles, 'score'), function(memo, p) { return memo + p }, 0)) / (_.size(actorProfiles)) : 0;
 
-                var certaintyLevel = (directorProfiles.length > 0 ? 3 : 0) + (genreProfiles.length > 0 ? 1 : 0) + (writerProfiles.length > 0 ? 2 : 0) + (actorProfiles.length > 0 ? 1 : 0) + (countryProfile[0] ? 1 : 0);
+                var certaintyLevel = (creatorProfiles.length > 0 ? 3 : 0) + (genreProfiles.length > 0 ? 1 : 0) + (writerProfiles.length > 0 ? 2 : 0) + (actorProfiles.length > 0 ? 1 : 0) + (countryProfile[0] ? 1 : 0);
 
                 done(null, {
                     certaintyLevel: certaintyLevel,
                     comments: allComments,
-                    score: certaintyLevel == 0 ? 0 : (((averageDirectors * 3) + (averageGenres) + (countryProfile[0] ? countryProfile[0].score : 0) + (averageActors) + (averageWriters * 2)) / certaintyLevel)
+                    score: certaintyLevel == 0 ? 0 : (((averageCreators * 3) + (averageGenres) + (countryProfile[0] ? countryProfile[0].score : 0) + (averageActors) + (averageWriters * 2)) / certaintyLevel)
                 });
             });
 
         })
-    }*/
+    }
 
     return {
         getAll: getAll,
