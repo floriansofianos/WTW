b588895ec317e8ce4bc5c593dc04762b142f1175
Florian Sofianos <florian.sofianos@gmail.com>
2018-02-25 13:21:57 +1000

    Add allow origin

--- wtw/server.js
+++ wtw/server.js
@@ -66,6 +66,13 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// CORS Enabled
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // set up the static html views
 app.use(express.static(__dirname + '/public'));
 
