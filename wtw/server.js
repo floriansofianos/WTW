125a52ba45cccc0cc4219937ea7488e040803729
Florian <florian.sofianos@fonebox.com.au>
2018-02-26 13:38:41 +1000

    CORS changes

--- wtw/server.js
+++ wtw/server.js
@@ -69,7 +69,8 @@ app.use(bodyParser.urlencoded({
 // CORS Enabled
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
     next();
 });
 
