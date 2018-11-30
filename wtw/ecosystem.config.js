8d8c53958562062b96a852e334b0a616ef88db31
Florian Sofianos <florian.sofianos@gmail.com>
2018-12-01 07:00:33 +1000

    First try with pm2

---
+++ wtw/ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [{
+    name: 'WTW',
+    script: 'server.js',
+    args: '',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+	  URL: 'https://app.whatowatch.net/'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
